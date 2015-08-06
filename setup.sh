b6eff54ce0f90af2f78773741270967d013ec73a
Peter Sujan <peterasujan@gmail.com>
2015-08-06 12:56:19 -0700

    Added emacs.d to setup.sh.

--- setup.sh
+++ setup.sh
@@ -13,7 +13,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="bash_aliases bashrc emacs gitconfig"    # list of files/folders to symlink in homedir
+files="bash_aliases bashrc emacs gitconfig emacs.d"    # list of files/folders to symlink in homedir
 
 ##########
 
